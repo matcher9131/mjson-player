870bfb86d3ac44b1b6a61afba731f9ba62380ffe
matcher9131 <masch9131cd@gmail.com>
2023-10-18 18:10:49 +0900

    Add components/TileContainer and components/TilePresenter

---
+++ src/components/TileContainer.tsx
@@ -0,0 +1,28 @@
+import { useRecoilValue } from "recoil";
+import { tileStateSelector } from "../modules/tileState/selectors";
+import TilePresenter from "./TilePresenter";
+
+const TileContainer = (tileId: number) => {
+    const tileState = useRecoilValue(tileStateSelector(tileId));
+    const svgTileId = tileState.isFacedown
+        ? "tile_facedown"
+        : tileId == 16
+        ? "tile4r"
+        : tileId == 52
+        ? "tile13r"
+        : tileId == 88
+        ? "tile22r"
+        : `tile${tileId >> 2}`;
+    return (
+        <TilePresenter
+            tileId={svgTileId}
+            x={tileState.x}
+            y={tileState.y}
+            tileRotate={tileState.isRotated ? 90 : 0}
+            sideRotate={-90 * tileState.sideIndex}
+            opacity={tileState.isInvisible ? 0 : 1}
+        />
+    );
+};
+
+export default TileContainer;
---
+++ src/components/TilePresenter.tsx
@@ -0,0 +1,20 @@
+type TilePresenterProps = {
+    readonly tileId: string;
+    readonly x: number;
+    readonly y: number;
+    readonly tileRotate: number;
+    readonly sideRotate: number;
+    readonly opacity: number;
+};
+
+const TilePresenter = ({ tileId, x, y, tileRotate, sideRotate, opacity }: TilePresenterProps) => {
+    return (
+        <use
+            xlinkHref={tileId}
+            transform={`rotate(${sideRotate}) translate(${x} ${y}) rotate(${tileRotate})`}
+            opacity={opacity}
+        />
+    );
+};
+
+export default TilePresenter;
