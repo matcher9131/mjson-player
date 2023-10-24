81f89c7ecc4f3937f07147daee4330c99b6b470e
matcher9131 <masch9131cd@gmail.com>
2023-10-24 14:13:27 +0900

    Add AtomEffect to modules/mJson/atoms for loading sample MJson

--- src/modules/mJson/atoms.ts
+++ src/modules/mJson/atoms.ts
@@ -1,13 +1,14 @@
 import { atom } from "recoil";
 import { MJson } from "../../types/mJson/mJson";
+import sampleMJson from "../../data/sample1.json";
 
 export const mJsonAtom = atom<MJson>({
     key: "mJsonAtom",
-    default: {} as MJson, // NOT IMPLEMENTED
+    default: {} as MJson,
     effects: [
         ({ setSelf, trigger }) => {
             if (trigger == "get") {
-                setSelf(/* */);
+                setSelf(sampleMJson as MJson);
             }
         },
     ],
