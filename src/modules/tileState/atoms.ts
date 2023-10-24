9f0a91ab951a130cbf8cdfd8a67a7ecf90a73e7d
matcher9131 <masch9131cd@gmail.com>
2023-10-24 14:16:56 +0900

    Add AtomEffect to modules/tileState/atoms for initialize TileState from MJson

--- src/modules/tileState/atoms.ts
+++ src/modules/tileState/atoms.ts
@@ -1,8 +1,18 @@
 import { atom } from "recoil";
 import { TileStateAtomType } from "./types";
+import { mJsonSelector } from "../mJson/selectors";
+import { createTileStates } from "./initialize";
 
 // atom[gameIndex][positionIndex][tileId]
 export const tileStateAtom = atom<TileStateAtomType>({
     key: "tileStateAtoms",
-    default: [], // NOT IMPLEMENTED
+    default: [],
+    effects: [
+        ({ setSelf, trigger, getLoadable }) => {
+            if (trigger == "get") {
+                const mJson = getLoadable(mJsonSelector).getValue();
+                setSelf(createTileStates(mJson));
+            }
+        },
+    ],
 });
